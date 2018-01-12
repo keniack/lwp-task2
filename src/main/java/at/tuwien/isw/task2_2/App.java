1559213ad63fe85a0fb1b08d0e7dd3b05582a46e
Cynthia Marcelino <cynthiakenia.arcanjomarcelino@drei.com>
2018-01-12 15:03:56 +0100

    initial commit

---
+++ src/main/java/at/tuwien/isw/task2_2/App.java
@@ -0,0 +1,13 @@
+package at.tuwien.isw.task2_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
